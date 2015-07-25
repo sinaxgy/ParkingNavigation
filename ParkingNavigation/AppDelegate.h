1ed737d2eb622614b76f16acc160af64de8edbba
徐成 <xucheng@xuchengdeMacBook-Air.local>
2015-07-25 19:42:25 +0800

    Inital commit

---
+++ ParkingNavigation/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  ParkingNavigation
+//
+//  Created by 徐成 on 15/7/5.
+//  Copyright (c) 2015年 徐成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ViewController.h"
+#import <MAMapKit/MAMapKit.h>
+
+#define APIKey @"c6a010e9d1e147319198817afd4de5d2"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
